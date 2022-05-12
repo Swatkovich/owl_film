d7993607b2aacc74922c4951b54543ae788479a4
Ivan Savenkov <swatkovich@yandex.ru>
2022-05-12 18:04:28 +0300

    navigation bar toggle fix

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,7 +1,8 @@
 import FormTab from "../../components/FormTab/FormTab"
+import styles from "./Form.module.css"
 
 export default function Form() {
   return (
-      <FormTab/>
+      <div className={styles.form_index}><FormTab/></div>
   );
 }

e5f7388ab1463c4341886b91a3eeeffa0cab9449
Ivan Savenkov <swatkovich@yandex.ru>
2022-05-19 10:33:05 +0300

    SocialTab and Decorations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import {Link, Outlet} from "react-router-dom";
 import SocialTab from "./components/SocialTab/SocialTab"
 import { useAuth } from './AuthContext';
+import MainPageButton from './components/MainPageButton/MainPageButton';
 
 function App() {
   const {role, exit} = useAuth();
@@ -11,7 +12,7 @@ function App() {
       <div className={'application'} id="application">
           <SocialTab className='social_tab_area'/>
            <div className='top_bar'>
-            <Link className='main_button' to="/Main"></Link>
+            <Link className='main_button' to="/Main"><MainPageButton/></Link>
             <nav className='nav_true' id='nav'>
               <Link className='link' to="/Form">Заявка</Link>
               <Link className='link' to="/Gallery">Галерея</Link>

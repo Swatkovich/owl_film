4c5427aea80a0b3bef541b969f3fb1d25bee2dd5
Ivan Savenkov <swatkovich@yandex.ru>
2022-05-20 13:19:15 +0300

    deploy #1

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { v4 } = require('uuid');
 
 const apiRouter = require("./api/api");
 
@@ -11,19 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
 
 app.listen(3001, () => {
   console.log(`Linstening port ${3001}`);

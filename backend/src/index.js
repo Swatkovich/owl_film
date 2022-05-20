63d3ff164ba8e25d96846005a0ac42d3f3a9dd20
Ivan Savenkov <swatkovich@yandex.ru>
2022-05-20 14:11:06 +0300

    deploy #3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const apiRouter = require("./api/api");
+const CLIENT_BULD_PATH = path.join(__dirname, '../../build')
 
 const app = express();
+
+app.use(express.static(CLIENT_BULD_PATH));
 
 app.use(cors());
 
@@ -11,8 +15,14 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
+app.get('*', (req, res) => {
+  this.response.sendFile(path.join(CLIENT_BULD_PATH));
+})
+
 app.listen(3001, () => {
   console.log(`Linstening port ${3001}`);
 });
 
 module.exports = app;
+
+
